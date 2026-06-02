2bcfb21517919d9f5bb2ff68dc31602b61dec3ca
homeromorozco-max <homeromorozco@gmail.com>
2026-06-02 15:57:30 -0300

    ghuoogyogohh

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,11 @@ let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
 
 let totalPorPersona: number = 0; // COMPLETAR
+if (esEstudiante === 'si')(
+totalPorPersona = ((Number(totalTexto)/10)*9)/Number(personasTexto)
+)
+else 
+totalPorPersona = (Number(totalTexto))/Number(personasTexto)
+
+
 console.log(`Total por persona: $${totalPorPersona}`);
